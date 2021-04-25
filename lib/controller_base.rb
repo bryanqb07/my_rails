46cbd8f237ecb2bcfdefb3eefc6d0c2b6f183afe
bryan lynch <bml312@nyu.edu>
2021-04-25 14:20:48 +0800

    add basic controller

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
@@ -8,20 +9,31 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response || false
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Double render detected." if already_built_response?
+    res['Location'] = url
+    res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Double render detected." if already_built_response?
+    res.write(content)
+    res['Content-Type'] = content_type
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
