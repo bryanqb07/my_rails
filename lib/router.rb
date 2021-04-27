3820ee18926419e5559c64ee04e682769fe03ba3
bryan lynch <bml312@nyu.edu>
2021-04-27 12:39:44 +0800

    add route params

--- lib/router.rb
+++ lib/router.rb
@@ -16,7 +16,10 @@ class Route
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
-    controller = controller_class.new(req, res, {})
+    match_data = @pattern.match(req.path)
+    route_params = Hash[match_data.names.zip(match_data.captures)]
+
+    controller = controller_class.new(req, res, route_params)
     controller.invoke_action(action_name)
   end
 end
@@ -36,20 +39,20 @@ class Router
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
-    # proc.call
+    instance_eval(&proc)
   end
 
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
-    define_method(http_method) do |pattern, method, controller_class, action_name|
-      add_route(pattern, method, controller_class, action_name)
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
   # should return the route that matches this request
   def match(req)
-    @routes.select { |route| route.matches?(req) }.first
+    @routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
