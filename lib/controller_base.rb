5e2aec8f8ed60c7468e4e1031b3888fabf900f52
bryan lynch <bml312@nyu.edu>
2021-06-29 18:46:59 +0800

    add flash class

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -3,6 +3,7 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require_relative './flash'
 
 class ControllerBase
   attr_reader :req, :res, :params
@@ -25,6 +26,7 @@ class ControllerBase
     res['Location'] = url
     res.status = 302
     session.store_session(res)
+    flash.store_flash(res)
     @already_built_response = true
   end
 
@@ -52,6 +54,11 @@ class ControllerBase
   # method exposing a `Session` object
   def session
     @session ||= Session.new(req)
+  end
+
+  # method exposing a `Flash` object
+  def flash
+    @flash ||= Flash.new(req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
